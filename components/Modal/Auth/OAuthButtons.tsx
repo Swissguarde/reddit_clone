8da9969b95911df1716dd666bc6a858e73b43be0
Kolade Ayomide <ayomikolade2003@gmail.com>
2023-02-08 19:43:34 +0000

    added google oauth

--- components/Modal/Auth/OAuthButtons.tsx
+++ components/Modal/Auth/OAuthButtons.tsx
@@ -1,12 +1,21 @@
-import { Button, Flex, Image } from "@chakra-ui/react";
-
+import { auth } from "@/firebase/clientApp";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 const OAuthButtons = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
   return (
     <Flex direction="column" width="100%" mb={4}>
-      <Button variant="oauth" mb={2}>
+      <Button
+        variant="oauth"
+        mb={2}
+        onClick={() => signInWithGoogle()}
+        isLoading={loading}
+      >
         <Image src="/images/googlelogo.png" height="20px" mr={4} />
         Continue with Google
       </Button>
+      {error && <Text>{error.message}</Text>}
     </Flex>
   );
 };
