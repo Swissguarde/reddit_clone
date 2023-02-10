328af385c5bc49966327a4545bf0cd37f256f65d
Kolade Ayomide <ayomikolade2003@gmail.com>
2023-02-10 15:35:14 +0000

    fixed styling & added added user proof components

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/firebase/clientApp";
 import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Directory from "./Directory/Directory";
 import RightContent from "./RightContent/RightContent";
 import SearchInput from "./SearchInput";
 
@@ -9,7 +10,12 @@ const Navbar = () => {
   const [user, loading, error] = useAuthState(auth);
 
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+      justify={{ md: "space-between" }}
+    >
       <Flex
         align="center"
         width={{ base: "40px", md: "auto" }}
@@ -23,8 +29,8 @@ const Navbar = () => {
           height="46px"
         />
       </Flex>
-      {/* <Directory/> */}
-      <SearchInput />
+      {user && <Directory />}
+      <SearchInput user={user} />
       <RightContent user={user} />
     </Flex>
   );
