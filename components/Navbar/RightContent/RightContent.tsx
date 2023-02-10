e38b42595739ed55bc798d3cf6ae8ae72d12bdcf
Kolade Ayomide <ayomikolade2003@gmail.com>
2023-02-10 14:33:10 +0000

    display user details

--- components/Navbar/RightContent/RightContent.tsx
+++ components/Navbar/RightContent/RightContent.tsx
@@ -4,9 +4,11 @@ import { Button, Flex } from "@chakra-ui/react";
 import { signOut, User } from "firebase/auth";
 import React from "react";
 import AuthButtons from "./AuthButtons";
+import Icons from "./Icons";
+import UserMenu from "./UserMenu";
 
 type RightContentProps = {
-  user: any;
+  user?: User | null;
 };
 
 const RightContent: React.FC<RightContentProps> = ({ user }) => {
@@ -14,11 +16,8 @@ const RightContent: React.FC<RightContentProps> = ({ user }) => {
     <>
       <AuthModal />
       <Flex justify="center" align="center">
-        {user ? (
-          <Button onClick={() => signOut(auth)}>Log Out</Button>
-        ) : (
-          <AuthButtons />
-        )}
+        {user ? <Icons /> : <AuthButtons />}
+        <UserMenu user={user} />
       </Flex>
     </>
   );
