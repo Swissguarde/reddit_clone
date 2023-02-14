292fbe6c1466a2931954f30dc293b137a590562e
Kolade Ayomide <ayomikolade2003@gmail.com>
2023-02-14 21:47:13 +0000

    ssr to fetch community pages

--- pages/r/[communityId]/index.tsx
+++ pages/r/[communityId]/index.tsx
@@ -1,8 +1,36 @@
+import { Community } from "@/atoms/communitiesAtom";
+import { firestore } from "@/firebase/clientApp";
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
 import React from "react";
+import safeJsonStringify from "safe-json-stringify";
 
-type indexProps = {};
-
-const index: React.FC<indexProps> = () => {
-  return <div>Have a good coding</div>;
+type CommunityPageProps = {
+  communityData: Community;
 };
-export default index;
+
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  return <div>{communityData.id}</div>;
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.communityId as string
+    );
+    const communityDoc = await getDoc(communityDocRef);
+
+    return {
+      props: {
+        communityData: JSON.parse(
+          safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+        ),
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+export default CommunityPage;
