12c4656a7bc022cb355023f591d5177ae18f767b
Kolade Ayomide <ayomikolade2003@gmail.com>
2023-03-02 19:19:25 +0000

    added useCommunityData custom hook

---
+++ hooks/useCommunityData.tsx
@@ -0,0 +1,144 @@
+import { authModalState } from "@/atoms/authModalAtom";
+import {
+  Community,
+  CommunitySnippet,
+  communityState,
+} from "@/atoms/communitiesAtom";
+import { auth, firestore } from "@/firebase/clientApp";
+import {
+  collection,
+  doc,
+  getDocs,
+  increment,
+  writeBatch,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilState, useSetRecoilState } from "recoil";
+
+const useCommunityData = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+  const [communityStateValue, setCommunityStateValue] =
+    useRecoilState(communityState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [user] = useAuthState(auth);
+
+  const onJoinOrLeaveCommunity = (
+    communityData: Community,
+    isJoined: boolean
+  ) => {
+    // is the user signed in?
+    // if not => open auth & prompt user to sign in
+
+    if (!user) {
+      setAuthModalState({ open: true, view: "login" });
+      return;
+    }
+
+    if (isJoined) {
+      leaveCommunity(communityData.id);
+      return;
+    }
+    joinCommunity(communityData);
+  };
+
+  const getMySnippets = async () => {
+    setLoading(true);
+    try {
+      // get user snippets
+      const snippetDocs = await getDocs(
+        collection(firestore, `users/${user?.uid}/communitySnippets`)
+      );
+      const snippets = snippetDocs.docs.map((doc) => ({ ...doc.data() }));
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        mySnippets: snippets as CommunitySnippet[],
+      }));
+    } catch (error: any) {
+      console.log("getMySnippetsError", error);
+      setError(error.message);
+    }
+    setLoading(false);
+  };
+
+  const joinCommunity = async (communityData: Community) => {
+    // batch write
+
+    try {
+      const batch = writeBatch(firestore);
+
+      // create a new community snippet
+      const newSnippet: CommunitySnippet = {
+        communityId: communityData.id,
+        imageURL: communityData.imageUrl || "",
+      };
+
+      batch.set(
+        doc(
+          firestore,
+          `users/${user?.uid}/communitySnippets`,
+          communityData.id
+        ),
+        newSnippet
+      );
+      // update no of members (1)
+      batch.update(doc(firestore, "communities", communityData.id), {
+        numberOfMembers: increment(1),
+      });
+      await batch.commit();
+
+      // update recoil state - community.mySnippets
+
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        mySnippets: [...prev.mySnippets, newSnippet],
+      }));
+    } catch (error: any) {
+      console.log("joinCommunityError", error);
+      setError(error.message);
+    }
+    setLoading(false);
+  };
+
+  const leaveCommunity = async (communityId: string) => {
+    // batch write
+    try {
+      const batch = writeBatch(firestore);
+
+      // delete community snippet from user
+      batch.delete(
+        doc(firestore, `users/${user?.uid}/communitySnippets`, communityId)
+      );
+      // update no of members (-1)
+      batch.update(doc(firestore, "communities", communityId), {
+        numberOfMembers: increment(-1),
+      });
+      await batch.commit();
+
+      // update recoil state - oommunity.mySnippets
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        mySnippets: prev.mySnippets.filter(
+          (item) => item.communityId !== communityId
+        ),
+      }));
+    } catch (error: any) {
+      console.log("leaveCommunityError", error);
+      setError(error.message);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (!user) return;
+    getMySnippets();
+  }, [user]);
+  return {
+    // data and functions
+    communityStateValue,
+    onJoinOrLeaveCommunity,
+    loading,
+  };
+};
+export default useCommunityData;
