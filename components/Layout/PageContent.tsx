29f98a28615471ed50e5e3f97eb8d2bc2681d6cf
Kolade Ayomide <ayomikolade2003@gmail.com>
2023-03-05 21:08:23 +0000

    updated styling

--- components/Layout/PageContent.tsx
+++ components/Layout/PageContent.tsx
@@ -6,16 +6,10 @@ interface PageContentProps {
 }
 const PageContent: React.FC<PageContentProps> = ({ children }) => {
   return (
-    <Flex border="1px solid red" justify="center" p="16px 0px">
-      <Flex
-        border="1px solid green"
-        width="95%"
-        justify="center"
-        maxWidth="860px"
-      >
+    <Flex justify="center" p="16px 0px">
+      <Flex width="95%" justify="center" maxWidth="860px">
         {/* LHS */}
         <Flex
-          border="1px solid blue"
           direction="column"
           width={{ base: "100%", md: "65%" }}
           mr={{ base: 0, md: 6 }}
@@ -25,7 +19,6 @@ const PageContent: React.FC<PageContentProps> = ({ children }) => {
 
         {/* RHS */}
         <Flex
-          border="1px solid orange"
           direction="column"
           display={{ base: "none", md: "flex" }}
           flexGrow={1}
