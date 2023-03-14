ddc1f023bc9967332b1182aae044dae5e1c7285f
Kolade Ayomide <ayomikolade2003@gmail.com>
2023-03-14 20:51:01 +0000

    added hook for posts functionality

--- hooks/usePosts.tsx
+++ hooks/usePosts.tsx
@@ -1,14 +1,35 @@
-import { postState } from "@/atoms/postAtom";
+import { Post, postState } from "@/atoms/postAtom";
+import { firestore, storage } from "@/firebase/clientApp";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 import { useRecoilState } from "recoil";
 
 const usePosts = () => {
   const [postStateValue, setPostStateValue] = useRecoilState(postState);
-  console.log(postStateValue);
 
   const onVote = async () => {};
   const onSelectPost = () => {};
 
-  const onDeletePost = async () => {};
+  const onDeletePost = async (post: Post): Promise<boolean> => {
+    try {
+      // check if post contains image, delete if exists
+      if (post.imageURL) {
+        const imageRef = ref(storage, `posts/${post.id}/image`);
+        await deleteObject(imageRef);
+      }
+      // delete post document from db
+      const postDocRef = doc(firestore, "posts", post.id!);
+      await deleteDoc(postDocRef);
+      // update recoil state
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: prev.posts.filter((item) => item.id !== post.id),
+      }));
+    } catch (error) {
+      return false;
+    }
+    return true;
+  };
 
   return {
     postStateValue,
